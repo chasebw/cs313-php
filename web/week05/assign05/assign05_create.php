86c19d07dcb522015ac4503dc6e7a137b3624882
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-04 15:49:16 -0700

    adding example

---
+++ web/week05/assign05/assign05_create.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assign05_create.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>Register for Account</h3>
+    <h4>Create an Account</h4>
+    <fieldset>
+      <input placeholder="Your First Name" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Last Name" type="text" tabindex="2" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Username" type="text" tabindex="3" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Password" type="text" tabindex="4" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Password Verification" type="text" tabindex="5" required autofocus>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+    <p class="copyright">Designed by a Student</p>
+    <p class="copyright"><a class="hoverclass" href="assign05_login.php">Login Page</a></p>
+  </form>
+</div>
+
+
+
+</body>
+</html>
---
+++ web/week05/assign05/assign05_login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assign05_login_style.css">
+    <script src="https://kit.fontawesome.com/d608f5aa42.js" crossorigin="anonymous"></script>
+    <link type="script" href="assign05_login_style.css">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <!-- Inspired  video from 'Amazing Transparent Login...' video on youtube-->
+    <div class="login_box">
+        <h1>Login</h1>
+        <div class="textbox">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Username" name="" value="">
+        </div>
+
+        <div class="textbox">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" name="" value="">
+        </div>
+
+        <input class="btn" type="button" name="" value="Sign In">
+        <button class="btn" type="button" name="" href='assign05_create.php' value="Create an Account">
+            <a class="" style= 'color: aliceblue' style="text-decoration: none" href="assign05_create.php">Create An Account</a></button>
+
+    </div>
+
+</body>
+
+</html>
