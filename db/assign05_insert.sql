a482021ed661611db1166ba891a1cee8f52f92cd
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-14 13:02:23 -0700

    adjusting results page

--- db/assign05_insert.sql
+++ db/assign05_insert.sql
@@ -7,7 +7,7 @@ INSERT INTO public.contact (first_name,last_name,user_id) VALUES
 
 
 INSERT INTO public.excercise_log (excercise_name,excercise_explain,record_date,user_id)
- VALUES('Pullups','3 sets of 15','02-06-2020', (SELECT user_id FROM public.user WHERE username = 'username47'));
+ VALUES('General Workout','3 sets of 15 situps, 4 sets of 16 pushups, 3 sets of 8 benchpress 215lbs','02-06-2020', (SELECT user_id FROM public.user WHERE username = 'username47'));
 
 
 INSERT INTO public.excercise_log (excercise_name,excercise_explain,record_date,user_id)
--- web/week05/assign05/results.php
+++ web/week05/assign05/results.php
@@ -16,13 +16,12 @@ $db = get_db();
 
 <?php
 
- $workout = "pullups";
+$workout = "pullups";
 
- if (isset($_POST["workout_type"])) { 
-     
-         $workout = $_POST["workout_type"];
- 
- }
+if (isset($_POST["workout_type"])) {
+
+    $workout = $_POST["workout_type"];
+}
 
 
 ?>
@@ -75,6 +74,9 @@ $statement->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Roboto:500i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Excercise Journal</title>
 </head>
@@ -124,6 +126,54 @@ $statement->execute();
         <p class="w3-container w3-teal confirming">All done</p>
         <form action="assign03_d.php" method="post">
             <button><a href="assign05_log.php">Return to Logging</a></button>
+            <button class="mys"><a style='color: white' style="text-decoration: none" href="assign05_options.php">Back to Menu</a></button>
+    </div>
+
+
+
+    <div class="container">
+        <h2>Your Workouts</h2>
+        <p>This table displays your workouts:</p>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Excercise Name:</th>
+                    <th>Excercise Description:</th>
+                    <th>Recorded Date:</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <?php $i = 0;     ?>
+                        <?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            // The variable "row" now holds the complete record for that
+                            // row, and we can access the different values based on their
+                            // name
+                            $excercise_name = $row['excercise_name'];
+                            $excercise_explain = $row['excercise_explain'];
+                            $record_date = $row['record_date'];
+                            echo "<tr>" . "<td>" . $excercise_name    . "</td>" .
+                                          "<td>" . $excercise_explain . "</td>" .
+                                          "<td>" . $record_date       . "</td>" .  "</tr>";
+                        }
+                        ?>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>
+                <tr>
+                    <td>Mary</td>
+                    <td>Moe</td>
+                    <td>mary@example.com</td>
+                </tr>
+                <tr>
+                    <td>July</td>
+                    <td>Dooley</td>
+                    <td>july@example.com</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 </body>
