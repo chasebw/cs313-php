059330726f71fa4fda63b9107c9795aa210ace2a
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-14 13:14:40 -0700

    fixing ui

--- web/week05/assign05/assign05_options.php
+++ web/week05/assign05/assign05_options.php
@@ -92,9 +92,9 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#"><strong>Home</strong></a></li>
+        <li><a href="results.php">View Workouts</a></li>
+        <li><a href="assign05_log.php">Log Workouts</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
--- web/week05/assign05/results.php
+++ web/week05/assign05/results.php
@@ -83,6 +83,30 @@ $statement->execute();
 
 <body>
 
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#"><strong>Home</strong></a></li>
+        <li><a href="results.php">View Workouts</a></li>
+        <li><a href="assign05_log.php">Log Workouts</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
     <header class="w3-container w3-teal">
         <h1>Excercise Journal</h1>
     </header>
