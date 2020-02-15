6d78269a20d163f59106b68c042f6427ec3ec7eb
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-15 10:03:13 -0700

    fix

--- web/week05/assign05/results.php
+++ web/week05/assign05/results.php
@@ -156,76 +156,7 @@ $statement->execute();
     <form action="assign03_d.php" method="post">
         <button type="button" class="btn btn-primary"><a href="assign05_log.php">Return to Logging</a></button>
         <button type="button" class="btn btn-primary"><a href="assign05_options.php">Back to Menu</a></button>
-        </div>
-
-
-        </div>
-      <!-- Grid column -->
-
-      <hr class="clearfix w-100 d-md-none pb-3">
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Information</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Find Friends</a>
-          </li>
-          <li>
-            <a href="#!">Motivation</a>
-          </li>
-          <li>
-            <a href="#!">Contact</a>
-          </li>
-          <li>
-            <a href="#!">Get Hired</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Helpful Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Follow Us</a>
-          </li>
-          <li>
-            <a href="#!">Information</a>
-          </li>
-          <li>
-            <a href="#!">Schedule</a>
-          </li>
-          <li>
-            <a href="#!">Compete</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-    </div>
-    <!-- Grid row -->
-
-  </div>
-  <!-- Footer Links -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href=""> My Workout</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
+    
 
 
 
