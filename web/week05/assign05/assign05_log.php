1526805db9092eec324e09cd1861d3b105ea2c57
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-15 09:56:48 -0700

    adding footers

--- web/week05/assign05/assign05_log.php
+++ web/week05/assign05/assign05_log.php
@@ -122,6 +122,75 @@ $db = get_db();
   </form>
 </div>
 
+</div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Information</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Find Friends</a>
+          </li>
+          <li>
+            <a href="#!">Motivation</a>
+          </li>
+          <li>
+            <a href="#!">Contact</a>
+          </li>
+          <li>
+            <a href="#!">Get Hired</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Helpful Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Follow Us</a>
+          </li>
+          <li>
+            <a href="#!">Information</a>
+          </li>
+          <li>
+            <a href="#!">Schedule</a>
+          </li>
+          <li>
+            <a href="#!">Compete</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href=""> My Workout</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
 </body>
 </html>
 
