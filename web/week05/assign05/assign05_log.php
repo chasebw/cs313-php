7ded4b04ab9f13e7aeb08fb6f022d2846faa3496
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-15 11:02:42 -0700

    adding delete

--- web/week05/assign05/assign05_log.php
+++ web/week05/assign05/assign05_log.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 /**********************************************************
@@ -18,126 +16,122 @@ $db = get_db();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:500i&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://kit.fontawesome.com/d608f5aa42.js" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" type="text/css" href="assign05_log.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:500i&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/d608f5aa42.js" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="assign05_log.css">
 
-    <style>
+  <style>
+    .navbar {
 
-      .navbar{
+      margin-bottom: 0;
+    }
 
-        margin-bottom: 0;
-      }
-
-        /* Add a gray background color and some padding to the footer */
+    /* Add a gray background color and some padding to the footer */
     footer {
       background-color: #f2f2f2;
       padding: 25px;
     }
 
-      .mar{
+    .mar {
 
-        margin-bottom: 5px;
-      }
+      margin-bottom: 5px;
+    }
 
-      body{
-        background: 'back.jpg' no-repeat;
-        background-size: cover;
-      }
+    body {
+      background: 'back.jpg'no-repeat;
+      background-size: cover;
+    }
+  </style>
 
-
-    </style>
-    
-    <title>Excercise Log</title>
+  <title>Excercise Log</title>
 </head>
+
 <body>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<body>
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <body>
 
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="assign05_options.php">Home</a></li>
+            <li><a href="results.php">View Workouts</a></li>
+            <li class="active"><a href="assign05_log.php">Log Workouts</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="assign05_login.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="w3-container w3-teal mar">
+      <h1>Excercise Log</h1>
+    </header>
+
+    <div class="container">
+      <form method='POST' action=insert.php>
+
+        <label for="workout_type">Workout Type</label>
+        <select name="name" id="workout_type">
+          <?php
+
+          try {
+            $statement = $db->prepare('Select DISTINCT excercise_name from excercise_log ORDER BY excercise_name');
+            $statement->execute();
+
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+              $excercise_name = $row['excercise_name'];
+              echo "<option value=$excercise_name>$excercise_name</option>";
+            }
+          } catch (Exception $ex) {
+
+            echo "$ex";
+          }
+
+          ?>
+
+        </select>
+
+        <label for="description">Description</label>
+        <textarea name="description" id="" cols="30" rows="10" required placeholder="3 sets of 15...."></textarea>
+
+
+        <!-- <input type="text" id="workout" name="workout" placeholder="Workout type"> --->
+        <button class="mys"><a style='color: white' style="text-decoration: none" href="assign05_options.php">Back to Menu</a></button>
+        <button class="mys"><a style='color: white' style="text-decoration: none" href="assign05_login.php">Back to Login</a></button>
+
+        <input type="submit" value="Submit">
+        <!-- Add link back to stuff here-->
+      </form>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="assign05_options.php">Home</a></li>
-        <li><a href="results.php">View Workouts</a></li>
-        <li class="active"><a href="assign05_log.php">Log Workouts</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="assign05_login.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<header class="w3-container w3-teal mar">
-<h1>Excercise Log</h1>
-</header>
-
-<div class="container">
-  <form method='POST' action = insert.php>
-    
-    <label for="workout_type">Workout Type</label>
-    <select name="name" id="workout_type">
-      <?php
-      
-      try{
-        $statement = $db -> prepare('Select DISTINCT excercise_name from excercise_log ORDER BY excercise_name');
-        $statement -> execute();
-        
-        while($row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-
-          $excercise_name = $row['excercise_name'];
-          echo "<option value=$excercise_name>$excercise_name</option>";
-
-        }
-      }
-      catch(Exception $ex){
-
-        echo "$ex";
-      }
-
-      ?>
-      
-    </select>
-
-    <label for="description">Description</label> 
-    <textarea name="description" id="" cols="30" rows="10" required placeholder="3 sets of 15...."></textarea>
-
-    
-   <!-- <input type="text" id="workout" name="workout" placeholder="Workout type"> --->
-   <button class="mys"><a style= 'color: white' style="text-decoration: none" href="assign05_options.php">Back to Menu</a></button>
-<button class="mys"><a style= 'color: white' style="text-decoration: none" href="assign05_login.php">Back to Login</a></button>
-    
-    <input type="submit" value="Submit">
-    <!-- Add link back to stuff here-->
-  </form>
-</div>
 
 
 
-</body>
-</html>
+  </body>
 
-
-
+  </html>
