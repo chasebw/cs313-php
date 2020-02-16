fc32511e47de10439b39dcd95e744df010ab94ea
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-15 18:54:11 -0700

    adding personal record

--- web/week05/assign05/insert_pr.php
+++ web/week05/assign05/insert_pr.php
@@ -6,6 +6,12 @@
     //We are going to want to sanitize this... ew
     $record_name = $_POST['record_name'];
     $record_amount = $_POST['record_amount'];
+
+    echo "record name is $record_name";
+    echo "record amount is $record_amount";
+
+
+
     $date = date('Y-m-d');
     $date = strval($date);
     echo "date is $date";
@@ -14,7 +20,8 @@
     // will need to store the username and password for this stuff in a session I think eventually
 
     try{
-    $insertEntry = $db->prepare("INSERT INTO public.personal_record (user_id,contact_id,record_name,record_amount,record_date)
+    $insertEntry = $db->prepare("INSERT INTO public.personal_record 
+    (user_id,contact_id,record_name,record_amount,record_date)
     VALUES 
     ((SELECT user_id FROM public.user WHERE username ='username47'),
     (SELECT contact_id FROM public.contact WHERE user_id= (SELECT user_id FROM public.user WHERE username = 'username47')),
