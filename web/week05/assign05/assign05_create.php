a86048135aa54806fd00a741c54f80881b0a0015
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-22 17:09:14 -0700

    changing this item

--- web/week05/assign05/assign05_create.php
+++ web/week05/assign05/assign05_create.php
@@ -81,8 +81,11 @@ button#sub_button, .login{
     <fieldset>
       <button type="button" name="submit_button" class="submit1" id="sub_button" value="submitform" onclick="check()">Submit</button>
     </fieldset>
-</form>
+    <fieldset>
     <button class="login"><a style="text-decoration: none" style="color: white" href="assign05_login.php">Login Page</a></button>
+    </fieldset>
+</form>
+    
 </div>
 
 
