f45b4940c717ef73a77fb4966d0b96dc80657d35
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-22 18:00:58 -0700

    changing this item

--- web/week05/assign05/assign05_options.php
+++ web/week05/assign05/assign05_options.php
@@ -135,7 +135,7 @@ $user_id = $_SESSION["user_id"];
 </nav>
 
 
-<header class="w3-container w3-top w3-white w3-xlarge w3-padding-16">
+<header class="w3-container w3-top top w3-white w3-xlarge w3-padding-16">
   <span class="w3-left w3-padding">SOME NAME</span>
 </header>
 
