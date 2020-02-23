86046ba5d33ffde643183230b0c19385e249d09a
chasebw <50175714+chasebw@users.noreply.github.com>
2020-02-22 18:04:56 -0700

    changing this item

--- web/week05/assign05/assign05_options.php
+++ web/week05/assign05/assign05_options.php
@@ -135,10 +135,6 @@ $user_id = $_SESSION["user_id"];
 </nav>
 
 
-<header class="w3-container w3-top top w3-white w3-xlarge w3-padding-16">
-  <span class="w3-left w3-padding">SOME NAME</span>
-</header>
-
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
